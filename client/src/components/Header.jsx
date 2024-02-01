c0985e233cafeac817dddb129a83729659cab0f6
Arvind <thakur122arvind@gmail.com>
2024-02-01 23:09:10 +0530

    create a test api route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
       <Link to="/">
         <h1 className="font-bold text-sm sm:text-xl flex felx-wrap">
-          <span className="text-slate-600">Arvind</span>
+          <span className="text-slate-600">Arvind</span> 
           <span className="text-red-600 ">Estate</span>
         </h1>
     </Link>
